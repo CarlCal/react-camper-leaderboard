bcc76786b0ff2a156e09095dd864d566aa6b07a5
Carl <callmer95@gmail.com>
2017-02-11 20:29:19 +0100

    working on the sorting

--- src/js/componants/Board.js
+++ src/js/componants/Board.js
@@ -8,7 +8,7 @@ export default class Board extends React.Component {
 
 	constructor() {
     super();
-    this.state = {user: ''};
+    this.state = {users: []};
   }
 
   componentDidMount() {
@@ -16,10 +16,25 @@ export default class Board extends React.Component {
   }
 
   getUser() {
-    return $.getJSON(' https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
-			      .then((data) => {
-			        this.setState({ user: data[0] });
-			      });
+	    return $.getJSON("https://fcctop100.herokuapp.com/api/fccusers/top/alltime")
+				      .then((data) => {
+				      	var arr = []
+
+				      	/*Check if for loop is quicker*/
+				      	data.forEach((element, i) => {
+				      		var user = {
+					      		key: (i + 1),
+					      		img: element.img,
+					      		name: element.username,
+					      		recent: element.recent,
+					      		alltime: element.alltime,
+					      	}
+
+					      	arr.push(user)
+
+					      	this.setState({users: arr})
+				      	})
+				      })
   }
 
 	render() {
@@ -30,12 +45,24 @@ export default class Board extends React.Component {
 				  <tr>
 				    <th>#</th>
 				    <th>Camper Name</th> 
-				    <th>Points in past 30 days</th>
-				    <th>All time points</th>
+					  <th><div class="radiowrapper" id="wrap1">
+							  	<input type="radio" name="sort" value="30Days" id="30-Days" />
+							  	<label for="30-Days">Points in past 30 days</label>
+								</div>
+						</th>
+				    <th><div class="radiowrapper" id="wrap2">
+						    	<input type="radio" name="sort" value="allTime" id="all-Time" />
+						    	<label for="all-Time">All time points</label>
+								</div>
+						</th>
 				  </tr>
 				  </thead>
 				  <tbody>
-				  	<Row rowContent={this.state.user}/>
+				  {
+				  	this.state.users.map((user, i) => {
+				  		return <Row key={i} rowContent={user}/>
+				  	})
+				  }
 				  </tbody>
 				</table>
 			</div>
--- src/js/componants/Row.js
+++ src/js/componants/Row.js
@@ -4,9 +4,9 @@ import React from "react"
 export default class Row extends React.Component {
 	render() {
 		return (
-			<tr>
-				<td>1</td>
-				<td>{this.props.rowContent.username}</td>
+			<tr> 
+				<td>{this.props.rowContent.key}</td>
+				<td><img class="profile-pic" src={this.props.rowContent.img} />{this.props.rowContent.name}</td>
 				<td>{this.props.rowContent.recent}</td>
 				<td>{this.props.rowContent.alltime}</td>
 			</tr>
